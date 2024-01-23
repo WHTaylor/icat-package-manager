848d993d43d06d9a32c83997d763d6aa5444995f
Will Taylor <william.h.taylor@stfc.ac.uk>
2024-01-23 21:37:26 +0000

    Allow SNAPSHOT versions to be installable

--- icatpackagemanager/repo.py
+++ icatpackagemanager/repo.py
@@ -1,6 +1,6 @@
 import shutil
 from pathlib import Path
-from typing import List
+from typing import List, Generator
 from urllib.request import urlopen
 
 import lxml.html
@@ -11,7 +11,7 @@ from .utils import Version
 _default_repo = "https://repo.icatproject.org/repo/org/icatproject"
 
 
-def _extract_content_list_from_table(page: str):
+def _extract_content_list_from_table(page: str) -> Generator[str, None, None]:
     parsed = lxml.html.fromstring(page)
     table_rows = parsed.xpath("body/table//tr")
     # First rows are headers/dividers, last is a divider
@@ -40,25 +40,41 @@ def get_component_versions(
         return [Version(v) for v in version_strings if Version.is_valid(v)]
 
 
+def get_distros(
+        component: str,
+        version: Version,
+        repo_url=_default_repo) -> List[str]:
+    with urlopen(f"{repo_url}/{component}/{version}") as f:
+        if f.status != 200:
+            raise Exception(f"Couldn't reach {repo_url}")
+        page = f.read().decode()
+        return [f for f in _extract_content_list_from_table(page)
+                if f.endswith("distro.zip")]
+
+
 def download_distro(
         component: str,
         version: Version,
         repo_url=_default_repo) -> Path:
+
+    fname = get_distro_name(component, version)
     if not version.is_snapshot():
-        fname = get_distro_name(component, version)
         url = f"{repo_url}/{component}/{version}/{fname}"
         print(f"Getting {url}")
-        dest = get_cache_destination(fname)
-
-        with urlopen(url) as response, open(dest, "wb") as f:
-            if response.status != 200:
-                raise Exception(f"Couldn't reach {repo_url}")
-            shutil.copyfileobj(response, f)
-        return dest
     else:
-        print("Need to handle dated snapshot versions")
-        print("Also good to allow LATEST version")
+        fname = get_distros(component, version)[0]
+        url = f"{repo_url}/{component}/{version}/{fname}"
+
+    dest = get_cache_destination(fname)
+
+    with urlopen(url) as response, open(dest, "wb") as f:
+        if response.status != 200:
+            raise Exception(f"Couldn't reach {repo_url}")
+        shutil.copyfileobj(response, f)
+    return dest
 
 
 if __name__ == "__main__":
-    download_distro("icat.server", Version("6.0.0"))
+    from pprint import pprint
+
+    pprint(download_distro("icat.server", Version("6.1.0-SNAPSHOT")))
